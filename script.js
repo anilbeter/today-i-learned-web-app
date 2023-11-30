7812572fdd9966213f3252c9bb059b540da83868
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-30 09:42:34 +0300

    add basic array & destructure

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ console.log(age1);
 const calcFactAge2 = year => new Date().getFullYear() - year;
 console.log(calcFactAge2(2015));
 // 8
+
+const fact = ["Lisbon is the capital of Portugal", 2015, true];
+console.log(fact[0]);
+// Lisbon is the capital of Portugal
+
+const [text, createdIn, isCorrect] = fact;
+console.log(text, createdIn, isCorrect);
+// Lisbon is the capital of Portugal 2015 true
+
+const [firstIndex] = fact;
+console.log(firstIndex);
+// Lisbon is the capital of Portugal
+
+// Spread operator
+const newFact = [...fact, "society"];
+console.log(newFact);
+// (4) ['Lisbon is the capital of Portugal', 2015, true, 'society']
