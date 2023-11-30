78c11f2c4598366df0daaedc2f9c32c5ebca3248
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-30 09:09:19 +0300

    create calcFactAge function

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ btn.addEventListener("click", function () {
     btn.textContent = "Share a fact";
   }
 });
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+  return age;
+}
+const age1 = calcFactAge(2015);
+console.log(age1);
+
+// console.log(new Date("2023-12-31"));
+// Sun Dec 31 2023 03:00:00 GMT+0300 (GMT+03:00)
