d40228676505334f284f0607a41e821e00f2132e
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-30 21:23:18 +0300

    write first component of react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <header class="header">
+      <div class="logo">
+        <img src="logo.png" alt="Today I Learned logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button class="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
 }
 
 export default App;
