2884c666d03bf5e395cbe0357b2e4cad2f127078
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-02 12:08:38 +0300

    create Loader component

--- src/App.js
+++ src/App.js
@@ -39,11 +39,14 @@ const initialFacts = [
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function getFacts() {
+      setIsLoading(true);
       const { data: facts, error } = await supabase.from("facts").select("*");
       setFacts(facts);
+      setIsLoading(false);
     }
     getFacts();
   }, []);
@@ -57,10 +60,14 @@ function App() {
 
       <main className="main">
         <CategoryFilter />
-        <FactList facts={facts} />
+        {isLoading ? <Loader /> : <FactList facts={facts} />}
       </main>
     </>
   );
+}
+
+function Loader() {
+  return <p>Loading...</p>;
 }
 
 function Header({ showForm, setShowForm }) {
