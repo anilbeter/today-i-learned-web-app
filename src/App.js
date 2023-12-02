bba7af70c96d17cf7e5398bd002778fc80832a67
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-02 10:23:36 +0300

    create form in react way

--- src/App.js
+++ src/App.js
@@ -70,10 +70,6 @@ function Header({ showForm, setShowForm }) {
   );
 }
 
-function NewFactForm() {
-  return <form className="fact-form">Fact form</form>;
-}
-
 const CATEGORIES = [
   { name: "technology", color: "#3b82f6" },
   { name: "science", color: "#16a34a" },
@@ -84,6 +80,25 @@ const CATEGORIES = [
   { name: "history", color: "#f97316" },
   { name: "news", color: "#8b5cf6" },
 ];
+
+function NewFactForm() {
+  return (
+    <form className="fact-form">
+      <input type="text" placeholder="Share a fact with the world!" />
+      <span>200</span>
+      <input type="text" placeholder="Trustworthy source..." />
+      <select>
+        <option value="">Choose category:</option>
+        {CATEGORIES.map(cat => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name[0].toUpperCase() + cat.name.slice(1)}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
+}
 
 function CategoryFilter() {
   return (
