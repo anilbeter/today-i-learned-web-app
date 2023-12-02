10902c4c6dd81402ebb7af72b259f3f4a31f4f13
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-02 12:02:18 +0300

    get data from supabase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import supabase from "./supabase";
 import "./style.css";
 
@@ -38,7 +38,15 @@ const initialFacts = [
 
 function App() {
   const [showForm, setShowForm] = useState(false);
-  const [facts, setFacts] = useState(initialFacts);
+  const [facts, setFacts] = useState([]);
+
+  useEffect(function () {
+    async function getFacts() {
+      const { data: facts, error } = await supabase.from("facts").select("*");
+      setFacts(facts);
+    }
+    getFacts();
+  }, []);
 
   return (
     <>
